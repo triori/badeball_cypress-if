91ea8df641dad569edf1f001f6b39355750dd8d3
slava.broit <slava.broit@aytm.com>
2024-03-19 22:58:18 -0400

    add: termianl report and cy if

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-
+import 'cypress-if'
+const options = {}
+require('cypress-terminal-report/src/installLogsCollector')(options);
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
